b5359ff59fb92bd5d945a6fdd4cd88344c0f6e6e
Wilson Brandão <wilsonbrandaocosta@hotmail.com>
2022-11-24 21:40:43 -0300

    Create react app

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+
+
+function App() {
+  return (
+    <p> abc</p>
+  );
+}
+
+export default App;
---
+++ src/components/HeaderComponent.jsx
@@ -0,0 +1,13 @@
+export function HeaderComponent() {
+    return (
+        <>
+            <img src="#" alt="" />
+            <ul>
+                <li>Home</li>
+                <li>Usuários</li>
+                <li>Detalhe</li>
+            </ul>
+
+        </>
+    )
+}
