ddc6cf0f38da2bb02ed1a1be7690bfcf3d8a42c7
Wilson Brandão <wilsonbrandaocosta@hotmail.com>
2022-12-05 22:26:57 -0300

    Feature ailtrar usuarios por tipo

--- src/components/Main/MainContent.jsx
+++ src/components/Main/MainContent.jsx
@@ -1,19 +1,223 @@
-import { useEffect, useState } from 'react';
+// import { useEffect, useState } from 'react';
+// import './MainContentStyle.scss';
+// import { UserContent } from './User/UserContent';
+
+// export function MainContent() {
+//     const [pageNumber, setpageNumber] = useState();
+
+//     const generateButtons = (CountItens) => {
+//         const MAX_BUTTONS = 10;
+
+//         let buttons = [];
+//         let count = 0;
+//         let countButtons = 0;
+
+//         //make an offset of 5 buttons
+//         if (pageNumber > 5) {
+//             count = pageNumber - 5;
+//         }
+
+//         while (countButtons < MAX_BUTTONS) {
+//             buttons.push(count);
+//             count++;
+//             countButtons++;
+//         }
+
+//         console.log(buttons)
+
+//         return buttons;
+
+//     }
+
+//     const [filterUser, setFilterUser] = useState({
+//         north: false,
+//         northeast: false,
+//         midwest: false,
+//         southeast: false,
+//         south: false
+//     })
+
+    
+//     const [filterUserType, setFilterUserType] = useState({
+//         special: false,
+//         normal: false,
+//         laborious: false
+//     })
+
+
+//     const toggleCheckbox = (item) => {
+//         const obj = { ...filterUser }
+//         obj[item] = !obj[item]
+//         setFilterUser(obj)
+//     }
+
+//     const toggleCheckboxType = (item) => {
+//         const obj = { ...filterUserType }
+//         obj[item] = !obj[item]
+//         setFilterUserType(obj)
+//     }
+
+
+//     return (
+//         <main >
+//             <form className='formFilter'>
+//                 <div className='filterContent'>
+//                     <fieldset >
+//                         <legend>Filtrar por usuário:</legend>
+
+//                         <div>
+//                             <input
+//                                 type="checkbox"
+//                                 id="special" name="special"
+//                                 onChange={() => toggleCheckboxType("special")}
+//                             />
+//                             <label for="special">Especial</label>
+//                         </div>
+
+//                         <div>
+//                             <input
+//                                 type="checkbox"
+//                                 id="normal"
+//                                 name="normal"
+//                                 onChange={() => toggleCheckboxType("normal")}
+//                             />
+//                             <label for="normal">Normal</label>
+//                         </div>
+
+//                         <div>
+//                             <input
+//                                 type="checkbox"
+//                                 id="laborious"
+//                                 name="laborious"
+//                                 onChange={() => toggleCheckboxType("laborious")}
+//                             />
+//                             <label for="laborious">Trabalhoso</label>
+//                         </div>
+
+//                     </fieldset>
+//                 </div>
+
+//                 <div className='filterContent'>
+//                     <fieldset>
+//                         <legend>Filtrar por região:</legend>
+
+//                         <div>
+//                             <input
+//                                 type="checkbox"
+//                                 id="north"
+//                                 name="north"
+//                                 onChange={() => toggleCheckbox("north")}
+//                             />
+//                             <label for="north">Norte</label>
+//                         </div>
+
+//                         <div>
+//                             <input
+//                                 type="checkbox"
+//                                 id="northeast"
+//                                 name="northeast"
+//                                 onChange={() => toggleCheckbox("northeast")}
+//                             />
+//                             <label for="northeast">Nordeste</label>
+//                         </div>
+
+//                         <div>
+//                             <input
+//                                 type="checkbox"
+//                                 id="midwest"
+//                                 name="midwest"
+//                                 onChange={() => toggleCheckbox("midwest")}
+//                             />
+//                             <label for="midwest">Centro-Oeste</label>
+//                         </div>
+
+//                         <div>
+//                             <input
+//                                 type="checkbox"
+//                                 id="southeast"
+//                                 name="southeast"
+//                                 onChange={() => toggleCheckbox("southeast")}
+//                             />
+//                             <label for="southeast">Sudeste</label>
+//                         </div>
+
+//                         <div>
+//                             <input
+//                                 type="checkbox"
+//                                 id="south"
+//                                 name="south"
+//                                 onChange={() => toggleCheckbox("south")}
+//                             />
+//                             <label for="south">Sul</label>
+//                         </div>
+
+//                     </fieldset>
+//                 </div>
+//             </form>
+//             <UserContent filterUsers={filterUser} filterUserType={filterUserType} pageNumber={pageNumber} generateButtons={generateButtons} />
+//             <div className='pageSelector'>
+//                 <ul>
+//                     <button onClick={(event) => setpageNumber(event.currentTarget.textContent)}>1</button>
+//                     <button onClick={(event) => setpageNumber(event.currentTarget.textContent)}>2</button>
+//                     <button onClick={(event) => setpageNumber(event.currentTarget.textContent)}>3</button>
+//                     <button onClick={(event) => setpageNumber(event.currentTarget.textContent)}>4</button>
+//                     <button onClick={(event) => setpageNumber(event.currentTarget.textContent)}>5</button>
+//                 </ul>
+//             </div>
+//         </main>
+//     )
+// }
+
+
+
+
+import { useState } from 'react';
 import './MainContentStyle.scss';
 import { UserContent } from './User/UserContent';
 
 export function MainContent() {
     const [pageNumber, setpageNumber] = useState();
+    const [buttonsCount, setbuttonsCount] = useState();
+
+    const generateButtons = (CountItens) => {
+        const MAX_BUTTONS = 10;
+
+        let buttons = [];
+        let count = 0;
+        let countButtons = 0;
+
+        if (pageNumber > 5) {
+            count = pageNumber - 5;
+        }
+
+        while (countButtons < MAX_BUTTONS) {
+            if (count > CountItens) {
+                break;
+            }
+
+            buttons.push(count);
+            count++;
+            countButtons++;
+
+        }
+
+
+        return buttons;
+    }
 
     const [filterUser, setFilterUser] = useState({
-        especial: false,
+        north: false,
+        northeast: false,
+        midwest: false,
+        southeast: false,
+        south: false
+    })
+
+    
+    const [filterUserType, setFilterUserType] = useState({
+        special: false,
         normal: false,
-        trabalhoso: false,
-        norte: false,
-        nordeste: false,
-        centroOeste: false,
-        sudeste: false,
-        sul: false
+        laborious: false
     })
 
 
@@ -21,6 +225,12 @@ export function MainContent() {
         const obj = { ...filterUser }
         obj[item] = !obj[item]
         setFilterUser(obj)
+    }
+
+    const toggleCheckboxType = (item) => {
+        const obj = { ...filterUserType }
+        obj[item] = !obj[item]
+        setFilterUserType(obj)
     }
 
 
@@ -34,10 +244,10 @@ export function MainContent() {
                         <div>
                             <input
                                 type="checkbox"
-                                id="especial" name="especial"
-                                onChange={() => toggleCheckbox("especial")}
+                                id="special" name="special"
+                                onChange={() => toggleCheckboxType("special")}
                             />
-                            <label for="especial">Especial</label>
+                            <label for="special">Especial</label>
                         </div>
 
                         <div>
@@ -45,7 +255,7 @@ export function MainContent() {
                                 type="checkbox"
                                 id="normal"
                                 name="normal"
-                                onChange={() => toggleCheckbox("normal")}
+                                onChange={() => toggleCheckboxType("normal")}
                             />
                             <label for="normal">Normal</label>
                         </div>
@@ -53,11 +263,11 @@ export function MainContent() {
                         <div>
                             <input
                                 type="checkbox"
-                                id="trabalhoso"
-                                name="trabalhoso"
-                                onChange={() => toggleCheckbox("trabalhoso")}
+                                id="laborious"
+                                name="laborious"
+                                onChange={() => toggleCheckboxType("laborious")}
                             />
-                            <label for="trabalhoso">Trabalhoso</label>
+                            <label for="laborious">Trabalhoso</label>
                         </div>
 
                     </fieldset>
@@ -70,64 +280,67 @@ export function MainContent() {
                         <div>
                             <input
                                 type="checkbox"
-                                id="norte"
-                                name="norte"
-                                onChange={() => toggleCheckbox("norte")}
+                                id="north"
+                                name="north"
+                                onChange={() => toggleCheckbox("north")}
                             />
-                            <label for="norte">Norte</label>
+                            <label for="north">Norte</label>
                         </div>
 
                         <div>
                             <input
                                 type="checkbox"
-                                id="nordeste"
-                                name="nordeste"
-                                onChange={() => toggleCheckbox("nordeste")}
+                                id="northeast"
+                                name="northeast"
+                                onChange={() => toggleCheckbox("northeast")}
                             />
-                            <label for="nordeste">Nordeste</label>
+                            <label for="northeast">Nordeste</label>
                         </div>
 
                         <div>
                             <input
                                 type="checkbox"
-                                id="centro-oeste"
-                                name="centro-oeste"
-                                onChange={() => toggleCheckbox("centroOeste")}
+                                id="midwest"
+                                name="midwest"
+                                onChange={() => toggleCheckbox("midwest")}
                             />
-                            <label for="centroOeste">Centro-Oeste</label>
+                            <label for="midwest">Centro-Oeste</label>
                         </div>
 
                         <div>
                             <input
                                 type="checkbox"
-                                id="sudeste"
-                                name="sudeste"
-                                onChange={() => toggleCheckbox("sudeste")}
+                                id="southeast"
+                                name="southeast"
+                                onChange={() => toggleCheckbox("southeast")}
                             />
-                            <label for="sudeste">Sudeste</label>
+                            <label for="southeast">Sudeste</label>
                         </div>
 
                         <div>
                             <input
                                 type="checkbox"
-                                id="sul"
-                                name="sul"
-                                onChange={() => toggleCheckbox("sul")}
+                                id="south"
+                                name="south"
+                                onChange={() => toggleCheckbox("south")}
                             />
-                            <label for="sul">Sul</label>
+                            <label for="south">Sul</label>
                         </div>
 
                     </fieldset>
                 </div>
             </form>
-            <UserContent filterUsers={filterUser} pageNumber={pageNumber} />
+            <UserContent filterUsers={filterUser} filterUserType={filterUserType} pageNumber={pageNumber} setbuttonsCount={generateButtons} />
             <div className='pageSelector'>
                 <ul>
-                    <button onClick={(event) => setpageNumber(event.currentTarget.textContent)}>1</button>
-                    <button onClick={(event) => setpageNumber(event.currentTarget.textContent)}>2</button>
-                    <button onClick={(event) => setpageNumber(event.currentTarget.textContent)}>3</button>
-                    <button onClick={(event) => setpageNumber(event.currentTarget.textContent)}>4</button>
-                    <button onClick={(event) => setpageNumber(event.currentTarget.textContent)}>5</button>
+
+                    {generateButtons().map((item) => {
+                        return (
+                            <li>
+                                <button onClick={() => setpageNumber(item)}>{item}</button>
+                            </li>
+                        )
+                    })}
                 </ul>
             </div>
         </main>
--- src/components/Main/User/UserContent.jsx
+++ src/components/Main/User/UserContent.jsx
@@ -4,39 +4,55 @@ import { User } from './User'
 // import customData from '../../../lib/UserRepository.json';
 
 
-export function UserContent({ filterUsers, pageNumber }) {
+export function UserContent({ filterUsers, filterUserType, pageNumber, setbuttonsCount }) {
     const [usersArray, setUsersArray] = useState([]);
-  
+
+
 
     useEffect(() => {
-        fetch(`http://localhost:8080/api/v1/users?page=${pageNumber? pageNumber : 1}&size=9`)
+        
+        console.log(filterUsers)
+        
+        const arrType = [];
+        for (const key in filterUserType) {
+            if (Object.hasOwnProperty.call(filterUserType, key)) {
+                const element = filterUserType[key];
+                if (element) arrType.push(key);
+            }
+        }
+        const filtraTipo = arrType.join(',');
+        
+        
+        
+        const arrRegion = [];
+        for (const key in filterUsers) {
+            if (Object.hasOwnProperty.call(filterUsers, key)) {
+                const element = filterUsers[key];
+                if (element) arrRegion.push(key);
+            }
+        }
+        const filtraRegion = arrRegion.join(',');
+        
+        console.log(filtraRegion)
+
+        let url = `http://localhost:8080/api/v1/users?page=${pageNumber ? pageNumber : 1}&size=9`
+        if (arrType.length > 0) url += `&types=` + filtraTipo
+        if (arrRegion.length > 0) url += `&regions=` + filtraRegion
+
+        fetch(url)
             .then(response => response.json())
-            .then(data => setUsersArray(data.users));
-    }, [pageNumber])
+            .then(data => {
+                setbuttonsCount(Math.ceil(data.totalCount / data.pageSize))
+                setUsersArray(data.users)
+            });
+        
+
+    }, [pageNumber, filterUsers, filterUserType])
 
 
     return (
-            <ul className='ListUsers'>
-
-                {usersArray
-                    .filter(user => filterUsers.norte ? user.location.region === 'NORTH' : false)
-                    .map(user => <User userInfo={user} />)}
-
-                {usersArray
-                    .filter(user => filterUsers.nordeste ? user.location.region === 'NORTHEAST' : false)
-                    .map(user => <User userInfo={user} />)}
-
-                {usersArray
-                    .filter(user => filterUsers.centroOeste ? user.location.region === 'MIDWEST' : false)
-                    .map(user => <User userInfo={user} />)}
-
-                {usersArray
-                    .filter(user => filterUsers.sudeste ? user.location.region === 'SOUTHEAST' : false)
-                    .map(user => <User userInfo={user} />)}
-
-                {usersArray
-                    .filter(user => filterUsers.sul ? user.location.region === 'SOUTH' : false)
-                    .map(user => <User userInfo={user} />)}
-            </ul>
+        <ul className='ListUsers'>
+            {usersArray?.map(user => <User userInfo={user} />)}
+        </ul>
     )
 }
